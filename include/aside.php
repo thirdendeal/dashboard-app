f3435b5a3646d5bd130fdb14651f424f51755aa7
Gabriel José <thirdendeal@protonmail.com>
2026-02-10 05:31:51 -0300

    Add sidebar layout

---
+++ include/aside.php
@@ -0,0 +1,17 @@
+<aside>
+  <h2 class="logotype">Burguer Burguer</h2>
+
+  <?php
+  $a  = $currentTab == 1 ? " tab__link--current"  : "";
+  $b  = $currentTab == 2 ? " tab__link--current"  : "";
+  $c  = $currentTab == 3 ? " tab__link--current"  : "";
+  ?>
+
+  <nav>
+    <ul class="tabs">
+      <li class="tab__item"><a href="/" class="tab__link<?= $a ?>">Dashboard</a></li>
+      <li class="tab__item"><a href="/fornecedores/" class="tab__link<?= $b ?>">Fornecedores</a></li>
+      <li class="tab__item"><a href="/produtos/" class="tab__link<?= $c ?>">Produtos</a></li>
+    </ul>
+  </nav>
+</aside>
--- index.php
+++ index.php
@@ -1,14 +1,32 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
   <title>Dashboard - Burguer Burguer</title>
+
+  <link rel="stylesheet" href="/css/partials/minireset.min.css">
+  <link rel="stylesheet" href="/css/partials/normalize.css">
+
+  <link rel="stylesheet" href="/css/layouts/default.css">
 </head>
 
 <body>
-  <p>Bem-vindo!</p>
+
+  <?php
+  $currentTab = 1;
+  include "include/aside.php";
+  ?>
+
+  <main>
+    <h1>Dashboard</h1>
+
+    <br>
+
+    <p>Bem-vindo!</p>
+  </main>
 </body>
 
 </html>
