2062d43d3a47a82dac74e9d0b841fd9a11ad796b
Gabriel José <thirdendeal@protonmail.com>
2026-02-23 15:36:57 -0300

    Refactor mirroring `fornecedores`

--- produtos/index.php
+++ produtos/index.php
@@ -1,3 +1,9 @@
+<?php
+require $_SERVER["DOCUMENT_ROOT"] . "/_model/database/pdo/get-rows.php";
+
+list($rows_success, $rows) = get_rows("produto");
+?>
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 
@@ -22,11 +28,60 @@
       <a href="/adicionar-produto/" class="green-button">Adicionar Produto</a>
       <br>
 
-      <div class="empty-view">
-        Nenhum produto encontrado :(
-      </div>
+      <?php if ($rows->rowCount() > 0) { ?>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Nome</th>
+              <th>Descrição</th>
+              <th>Código</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php while ($row = $rows->fetch(PDO::FETCH_ASSOC)) { ?>
+              <tr class="link-row" data-href="/produto?id=<?= $row["id_produto"] ?>">
+                <td><?= $row["id_produto"] ?></td>
+                <td><?= $row["nome"] ?></td>
+                <td><?= $row["descrição"] ?></td>
+                <td class="<?php echo $row["código"] ? "" : "gray" ?>">
+                  <?php echo $row["código"] ? $row["código"] : "(Nenhum)" ?>
+                </td>
+                <td class="<?php echo $row['status'] ? 'green' : 'red' ?>">
+                  <?php echo $row["status"] ? "ATIVO" : "INATIVO" ?>
+                </td>
+              </tr>
+            <?php } ?>
+          </tbody>
+        </table>
+      <?php } else { ?>
+        <div class="empty-view">
+          Nenhum produto encontrado :(
+        </div>
+      <?php } ?>
     </div>
   </main>
+
+  <script src="/_view/vendor/jquery-v4.0.0.min.js"></script>
+
+  <script>
+    $(document).ready(function() {
+      $(".link-row").hover(function() {
+        $(this).children().each(function() {
+          $(this).css("background-color", "blanchedalmond");
+        });
+      }, function() {
+        $(this).children().each(function() {
+          $(this).removeAttr('style');
+        });
+      });
+
+      $(".link-row").click(function() {
+        window.location = $(this).data("href");
+      });
+    });
+  </script>
 </body>
 
 </html>
